fa38b40d0cd4bafa00647395026f9af7e6b52ba2
Juan Fumero <jjfumero@gmail.com>
2022-11-09 11:25:09 +0100

    command documentation fixed

--- commands.sh
+++ commands.sh
@@ -5,9 +5,9 @@ tornado -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.HelloT
 
 
 ## Running Mandelbrot 
-tornado --threadInfo -Ds0.t0.device=0:0 -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --tornado
+tornado --threadInfo --jvm="-Ds0.t0.device=0:0" -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --params="--tornado"
 
 
 ## Run Blur Filter 
-tornado --threadInfo -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.BlurFilter --tornado
+tornado --threadInfo -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.BlurFilter --params="--tornado"
 
