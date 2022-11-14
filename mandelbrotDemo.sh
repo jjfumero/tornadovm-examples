0926b7c3ab0fbc07ed97472a24bdd22e49a6efc6
Juan Fumero <jjfumero@gmail.com>
2022-11-14 11:56:44 +0100

    Scripts fixed

--- mandelbrotDemo.sh
+++ mandelbrotDemo.sh
@@ -4,21 +4,21 @@
 tornado --devices 
 
 ## Run the multi-thread version for reference 
-tornado -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --mt
+tornado -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --params="--mt"
 
 ## Run the TornadoVM Version (it will select the device 0:0 by default)
-tornado -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --tornado
+tornado -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --params="--tornado"
 
 ## Print the device and thread information in which the application is running
-tornado --threadInfo -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --tornado
+tornado --threadInfo -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --params="--tornado"
 
 ## Get the SPIRV code (assuming the SPIRV backend is installed in the device 0:0)
-tornado --debug --threadInfo -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --tornado
+tornado --debug --threadInfo -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --params="--tornado"
 spirv-dis <spirv-binary> 
 
 ## Change the device
-tornado --threadInfo -Ds0.t0.device=1:1 -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --tornado
+tornado --threadInfo -Ds0.t0.device=1:1 -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --params="--tornado"
 
 ## Run with the tornadoVM profiler
-tornado --enableProfiler console --threadInfo -Ds0.t0.device=0:0 -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --tornado
+tornado --enableProfiler console --threadInfo -Ds0.t0.device=0:0 -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.Mandelbrot --params="--tornado"
 
