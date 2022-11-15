1aa9a212271462825d76a3c4890d82c0e4d9a76a
Juan Fumero <juan.fumero@manchester.ac.uk>
2022-11-15 17:14:17 +0000

    runClient and runServer scripts added

---
+++ runClient.sh
@@ -0,0 +1,1 @@
+tornado -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.taskmigration.Client 
---
+++ runServer.sh
@@ -0,0 +1,1 @@
+tornado --printKernel --threadInfo -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.taskmigration.Server 
