925f61443a24197ee31c74cab315b05c581b78f4
Juan Fumero <jjfumero@gmail.com>
2023-10-10 17:12:34 +0200

    Example using vector types

---
+++ src/main/java/io/github/jjfumero/HelloVectors.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 Juan Fumero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.jjfumero;
+
+import uk.ac.manchester.tornado.api.TaskGraph;
+import uk.ac.manchester.tornado.api.TornadoExecutionPlan;
+import uk.ac.manchester.tornado.api.annotations.Parallel;
+import uk.ac.manchester.tornado.api.collections.types.Float4;
+import uk.ac.manchester.tornado.api.collections.types.VectorFloat4;
+import uk.ac.manchester.tornado.api.common.TornadoDevice;
+import uk.ac.manchester.tornado.api.enums.DataTransferMode;
+
+/**
+ * tornado --threadInfo -cp target/tornadovm-examples-1.0-SNAPSHOT.jar io.github.jjfumero.HelloVectors
+ */
+public class HelloVectors {
+
+    public static void parallelInitialization(VectorFloat4 data) {
+        for (@Parallel int i = 0; i < data.size(); i++) {
+            data.set(i, new Float4(i, i, i, i));
+        }
+    }
+
+    public static void computeSquare(VectorFloat4 data) {
+        for (@Parallel int i = 0; i < data.size(); i++) {
+            Float4 item = data.get(i);
+            Float4 result = Float4.mult(item, item);
+            data.set(i, result);
+        }
+    }
+
+    public static void main( String[] args ) {
+        VectorFloat4 array = new VectorFloat4(128);
+        TaskGraph taskGraph = new TaskGraph("s0")
+                .transferToDevice(DataTransferMode.EVERY_EXECUTION, array)
+                .task("t0", HelloVectors::parallelInitialization, array)
+                .task("t1", HelloVectors::computeSquare, array)
+                .transferToHost(DataTransferMode.EVERY_EXECUTION, array);
+
+        TornadoExecutionPlan executionPlan = new TornadoExecutionPlan(taskGraph.snapshot());
+
+        // Obtain a device from the list
+        TornadoDevice device = TornadoExecutionPlan.getDevice(0, 0);
+        executionPlan.withDevice(device);
+
+        // Execute the application
+        executionPlan.execute();
+
+    }
+}
