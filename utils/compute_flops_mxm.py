4f50287c7036cf996703f827b0874dfe9ed5c0ef
Juan Fumero <jjfumero@gmail.com>
2024-12-19 10:58:25 +0100

    [script] compute flops added

---
+++ utils/compute_flops_mxm.py
@@ -0,0 +1,29 @@
+# #################################################################################################
+# Simple program to calculate the Floating point operations per second for the Matrix Multiplication
+# #################################################################################################
+
+import argparse
+
+def parseArguments():
+    parser = argparse.ArgumentParser(prog='compute_flops_mxm',
+                    description='Compute the Floating Point Operations per Second for the matrix multiplication given the time and the input matrix size',
+                    epilog='-h for help')
+
+    parser.add_argument('--size', dest="size", type=float, default=None, help="Set the input matrix size (size x size)")
+    parser.add_argument('--time', dest="time", type=float, default=None, help="Set the time in nanoseconds (ns)")
+
+    args = parser.parse_args()
+    return args
+
+def computeFlops(args):
+    matrixSize = args.size 
+    nanosecondsSolution = args.time
+    numFloatOperationsPerKernel = 2 * matrixSize**3 
+    timeScaleSec = 1E9
+    gigaflops = 1E-9 * numFloatOperationsPerKernel / (nanosecondsSolution / timeScaleSec)
+    print("GigaGlops: " + str(round(gigaflops, 2)))
+
+
+if __name__ == "__main__":
+    args = parseArguments()
+    computeFlops(args)
