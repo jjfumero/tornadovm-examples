961ba7b3f623dc2fbcf2c432a1815e34367fd13f
Juan Fumero <jjfumero@gmail.com>
2024-12-19 11:00:00 +0100

    minor change

--- utils/compute_flops_mxm.py
+++ utils/compute_flops_mxm.py
@@ -5,15 +5,11 @@
 import argparse
 
 def parseArguments():
-    parser = argparse.ArgumentParser(prog='compute_flops_mxm',
-                    description='Compute the Floating Point Operations per Second for the matrix multiplication given the time and the input matrix size',
-                    epilog='-h for help')
-
+    parser = argparse.ArgumentParser(prog='compute_flops_mxm', 
+                description='Compute the Floating Point Operations per Second for the matrix multiplication given the time and the input matrix size')
     parser.add_argument('--size', dest="size", type=float, default=None, help="Set the input matrix size (size x size)")
     parser.add_argument('--time', dest="time", type=float, default=None, help="Set the time in nanoseconds (ns)")
-
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 def computeFlops(args):
     matrixSize = args.size 
@@ -22,7 +18,6 @@ def computeFlops(args):
     timeScaleSec = 1E9
     gigaflops = 1E-9 * numFloatOperationsPerKernel / (nanosecondsSolution / timeScaleSec)
     print("GigaGlops: " + str(round(gigaflops, 2)))
-
 
 if __name__ == "__main__":
     args = parseArguments()
